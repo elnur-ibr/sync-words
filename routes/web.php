8c3569a05e0e62c38f3663dff1a2097f249b217a
Elnur Ibrahim-zada <e.ibrahimzade@gmail.com>
2023-07-25 10:30:16 +0400

    Cleared TODOs

--- routes/web.php
+++ routes/web.php
@@ -12,11 +12,3 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
-//TODO delete
-Route::get('test', function () {
-    dd(
-        now()->subDays(3),
-
-    );
-});
